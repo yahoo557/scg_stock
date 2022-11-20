f1b3a59fe7e7bf764ca6e3fafdc55ec9c69772eb
Seungbaek Lee <yahoo557@naver.com>
2022-11-20 17:09:44 +0900

    df 출력 에러

--- test/일평균수출액과 코스피50/main.py
+++ test/일평균수출액과 코스피50/main.py
@@ -12,7 +12,7 @@ import pandas
 data_dir = os.path.dirname(os.path.abspath(__file__))+"/Data/"
 export = pandas.read_csv(data_dir+"수출입총괄_20221023000003.csv", encoding='UTF8')
 
-# export = export.values.tolist()[0][1:]
+
 
 
 # 2. 코스피 50 각 종목의 월별 시가총액 평균을 구한다.
@@ -22,23 +22,33 @@ kospi50_code = [ '005930', '373220', '000660', '207940', '051910', '006400', '00
                 '034020', '259960', '009150', '011200', '051900', '329180', '036570', '018260', '010950', '003490', 
                 '030200', '009830', '000810', '316140', '377300', '066970', '024110', '011070', '302440', '090430' ]
 
-print(fdr.DataReader('373220', '2000-02-01', '2022-10-05'))
+
+
+# 2. 해당 년도에 상장되었는지 확인하는 함수
+
+# 현재 코스피 50의 각 종목별 상장일자가 다르기때문에 데이터의 최초 일자가 다 다르기때문,
+def checkIsOpened(code, year):
+    kospi_df = fdr.DataReader(code, '2000-02-01', '2022-10-05');
+    for index in kospi_df.index:
+        if str(index)[:4] == year:
+          return True
+    return False
+
+
+
 # 3. 각 연도 마다 월별 시가총액의 평균을 구한다.
 def getMonthlyTotal(code,year):
-    
-    if(year == 0):
-        kospi_df = fdr.DataReader(code, '2000-02-01', '2022-10-05');
-    else:
-        kospi_df = fdr.DataReader(code, start , end)
-    
+    start = year+"-01-01"
+    end = year+"-12-31"
+    kospi_df = fdr.DataReader(code, start, end);
     kospi_month = []
     
+
     # 월별 시가총액평균
     this_month = kospi_df.index[0].month
     cnt = 0
     temp = 0
 
-    
     for i in range(len(kospi_df)):
         if kospi_df.index[i].month == this_month :
             temp += kospi_df['Close'][i]*kospi_df['Volume'][i]/1000000
@@ -48,13 +58,11 @@ def getMonthlyTotal(code,year):
             this_month = kospi_df.index[i].month
             cnt = 1
             temp = kospi_df['Close'][i]*kospi_df['Volume'][i]/1000000
-
+    kospi_month.append(int(temp/cnt))
     return kospi_month
 
 # 4. 각 연도별 일평균 수출액을 구한다.
 def getExportYear(year):
-    export = pandas.read_csv(data_dir+"수출입총괄_20221023000003.csv", encoding='UTF8')
-
     for i in range(1, len(export.columns)):
         if(export.columns[i][:4]==str(year)):
             return export.values.tolist()[0][i:i+12]
@@ -65,17 +73,22 @@ def getExportYear(year):
 
 
 
-
-
 # 3,4에서 구한 데이터로 상관계수를 구해서 dataframe에 저장한다.
-df = pandas.DataFrame(index=range(2001, 2022), columns=kospi50_code);
-for year in range(2001, 2022):
-    for code in kospi50_code:
-        print("len(getMonthlyTotal(code,str(year))) = ", len(getMonthlyTotal(code,str(year))) )
-        print("len(getExportYear(year)) = ", len(getExportYear(year)) )
-        # df[year][code] = scs.pearsonr(getMonthlyTotal(code,str(year)), getExportYear(year)).statistic
+df = pandas.DataFrame(index=range(2001, 2021), columns=kospi50_code);
+processCnt = 1
+for year in range(2001, 2021):
+        for code in kospi50_code:
+            if(checkIsOpened(code, str(year))):
+                monthlyTotal = getMonthlyTotal(code,str(year))
+                exportYear = getExportYear(year)
+                if (len(monthlyTotal) == len (exportYear)):
+                    df.loc[str(year), code] = scs.pearsonr(monthlyTotal, exportYear).statistic
+            print(processCnt/100)    
+            processCnt+= 1
 
-# print(df)
+
+
+print(df)
 
 
 
